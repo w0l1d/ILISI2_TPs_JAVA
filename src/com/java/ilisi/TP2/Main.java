b1bf6806e55adeb75e5aa67ba6157f13e7826994
W0L1D <ahdoufwalid@gmail.com>
2022-12-13 09:16:54 +0100

    hotel save and load from file (√ tested)
    enhanced menus

--- src/com/java/ilisi/TP2/Main.java
+++ src/com/java/ilisi/TP2/Main.java
@@ -1,10 +1,11 @@
 package com.java.ilisi.TP2;
 
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-
     private static final Scanner in = new Scanner(System.in);
     public static String tronque(double dbl, int dLimit) {
         String d = String.valueOf(dbl);
@@ -14,28 +15,18 @@ public class Main {
         return d.substring(0, i + 1 + dLimit);
     }
     public static void main(String[] args) {
-//        System.out.print("> ");
-//        String line = in.nextLine();
-//        StringTokenizer st = new StringTokenizer(line, " ");
-//        while (st.hasMoreTokens()) {
-//            try {
-//                System.out.println(tronque(Double.parseDouble(st.nextToken()), Integer.parseInt(st.nextToken())));
-//            } catch (NumberFormatException e) {
-//                System.err.println("Error : Format du premier double ou entier incorrect");
-//            } catch (NoSuchElementException e) {
-//                System.err.println("Error : entier n'est pas fourni");
-//            }
-//        }
-
-        String s = in.nextLine();
-        System.out.println("chaine : '"+s+"'");
-        if (s.isEmpty())
-                System.out.println("empty string");
-        else
-            System.out.println("not empty string");
-
-        if (s.equals("\n"))
-            System.out.println("== \\n");
+        System.out.print("> ");
+        String line = in.nextLine();
+        StringTokenizer st = new StringTokenizer(line, " ");
+        while (st.hasMoreTokens()) {
+            try {
+                System.out.println(tronque(Double.parseDouble(st.nextToken()), Integer.parseInt(st.nextToken())));
+            } catch (NumberFormatException e) {
+                System.err.println("Error : Format du premier double ou entier incorrect");
+            } catch (NoSuchElementException e) {
+                System.err.println("Error : entier n'est pas fourni");
+            }
+        }
 
     }
 }
